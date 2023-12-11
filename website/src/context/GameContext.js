df65f1e9ffd2375a7259fca55aedc7682fc10a68
David <rajlic.david@gmail.com>
2023-12-11 12:30:33 +0100

    restructed a lot, finished a functionlity of check and passant

---
+++ website/src/context/GameContext.js
@@ -0,0 +1,20 @@
+import React, { createContext, useReducer } from 'react';
+import GameReducer from './GameReducer';
+
+const initialState = {
+	possibleMoves: [],
+	turn: 'w', //w or b
+	check: false, //true if the side to move (current turn) is in check.
+};
+
+export const GameContext = createContext(initialState);
+
+export const GameProvider = ({ children }) => {
+	const [state, dispatch] = useReducer(GameReducer, initialState);
+
+	return (
+		<GameContext.Provider value={{ ...state, dispatch }}>
+			{children}
+		</GameContext.Provider>
+	);
+};
