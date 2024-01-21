e3134bb226b54e4362250b5861bed3f2fce04ceb
Asos75 <andraz.sosteric@gmail.com>
2024-01-21 14:51:03 +0100

    update

--- Engine/chessMain.cpp
+++ Engine/chessMain.cpp
@@ -3,6 +3,8 @@
 #include <string>
 #include <iostream>	
 #include "chessBoard.h"
+#include "engine.h"
+
 void bufferToString(char buffer[64], size_t size, std::string& position){
 	position = "";
 	for(int i = 0; i < size; i++){
@@ -16,9 +18,9 @@ napi_value Find(napi_env env, napi_callback_info info){
 	napi_value output;
 	napi_get_cb_info(env, info, &argc, args, NULL, NULL); //Retrieves arguments about the function call from node application
 	
-	if(argc > 1){ //Checks argument count and throws error if incorrect
+	if(argc != 2){ //Checks argument count and throws error if incorrect
 		napi_value err_info;
-		char* err_txt = "To many arguments";
+		char* err_txt = "Err: argc";
 		napi_create_string_utf8(env, err_txt, sizeof(err_txt), &err_info);
 		napi_throw(env, err_info);
 	}
@@ -37,7 +39,8 @@ napi_value Find(napi_env env, napi_callback_info info){
 	std::cout << chessBoard.checkCheckmate(chessBoard.findPiece('K')) << std::endl;
 	std::cout << "Native: ";	//Troubleshooting code
 	std::cout << position << std::endl; //Troubleshooting code
-	
+
+
 	napi_create_string_utf8(env, buffer, copiedSize, &output); //building the string to return
 	return output;
 }
